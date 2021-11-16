aa32a533fbab2fe942acad46c70411d44ded0eaf
aso2001036 <83050405+aso2001036@users.noreply.github.com>
2021-11-16 10:44:26 +0900

    Add files via upload

---
+++ ソースコード/Q&A/Q&A.php
@@ -0,0 +1,193 @@
+<?php
+session_start();
+?>
+<?php require '../header.php';?>
+<div class="qora">
+<h1 class="him">Q&A  よくある質問</h1>
+<div class="qa-list mts">
+    <dl class="qa">
+        <dt>○○○○○○○○○○○○について</dt>
+        <dd>
+            <div class="talking">
+                <figure class="talking-left_icon">
+                    <img src="img/icon_psyduck.jpg" alt="" class="image_p">
+                    <figcaption>user</figcaption>
+                </figure>
+                <div class="talking-left">
+                    <p>○○○○○○○○○○○○、<br>
+                        ○○○○○○○○○○○○ですか？</p>
+                </div>
+            </div>
+            <div class="talking">
+                <figure class="talking-right_icon">
+                    <img src="img/icon_meowth.jpg" alt="" class="image_m">
+                    <figcaption>aSONY</figcaption>
+                </figure>
+                <div class="talking-right">
+                    <p>○○○○○○○○○○○○、<br>
+                        ○○○○○○○○○○○○ですよ。</p>
+                </div>
+            </div>
+        </dd>
+    </dl>
+    <dl class="qa">
+        <dt>○○○○○○○○○○○○について</dt>
+        <dd>
+            <div class="talking">
+                <figure class="talking-left_icon">
+                    <img src="img/icon_psyduck.jpg" alt="" class="image_p">
+                    <figcaption>user</figcaption>
+                </figure>
+                <div class="talking-left">
+                    <p>○○○○○○○○○○○○、<br>
+                        ○○○○○○○○○○○○ですか？</p>
+                </div>
+            </div>
+            <div class="talking">
+                <figure class="talking-right_icon">
+                    <img src="img/icon_meowth.jpg" alt="" class="image_m">
+                    <figcaption>aSONY</figcaption>
+                </figure>
+                <div class="talking-right">
+                    <p>○○○○○○○○○○○○、<br>
+                        ○○○○○○○○○○○○ですよ。</p>
+                </div>
+            </div>
+        </dd>
+    </dl>
+    <dl class="qa">
+        <dt>○○○○○○○○○○○○について</dt>
+        <dd>
+            <div class="talking">
+                <figure class="talking-left_icon">
+                    <img src="img/icon_psyduck.jpg" alt="" class="image_p">
+                    <figcaption>user</figcaption>
+                </figure>
+                <div class="talking-left">
+                    <p>○○○○○○○○○○○○、<br>
+                        ○○○○○○○○○○○○ですか？</p>
+                </div>
+            </div>
+            <div class="talking">
+                <figure class="talking-right_icon">
+                    <img src="img/icon_meowth.jpg" alt="" class="image_m">
+                    <figcaption>aSONY</figcaption>
+                </figure>
+                <div class="talking-right">
+                    <p>○○○○○○○○○○○○、<br>
+                        ○○○○○○○○○○○○ですよ。</p>
+                </div>
+            </div>
+        </dd>
+    </dl>
+    <dl class="qa">
+        <dt>○○○○○○○○○○○○について</dt>
+        <dd>
+            <div class="talking">
+                <figure class="talking-left_icon">
+                    <img src="img/icon_psyduck.jpg" alt="" class="image_p">
+                    <figcaption>user</figcaption>
+                </figure>
+                <div class="talking-left">
+                    <p>○○○○○○○○○○○○、<br>
+                        ○○○○○○○○○○○○ですか？</p>
+                </div>
+            </div>
+            <div class="talking">
+                <figure class="talking-right_icon">
+                    <img src="img/icon_meowth.jpg" alt="" class="image_m">
+                    <figcaption>aSONY</figcaption>
+                </figure>
+                <div class="talking-right">
+                    <p>○○○○○○○○○○○○、<br>
+                        ○○○○○○○○○○○○ですよ。</p>
+                </div>
+            </div>
+        </dd>
+    </dl>
+    <dl class="qa">
+        <dt>○○○○○○○○○○○○について</dt>
+        <dd>
+            <div class="talking">
+                <figure class="talking-left_icon">
+                    <img src="img/icon_psyduck.jpg" alt="" class="image_p">
+                    <figcaption>user</figcaption>
+                </figure>
+                <div class="talking-left">
+                    <p>○○○○○○○○○○○○、<br>
+                        ○○○○○○○○○○○○ですか？</p>
+                </div>
+            </div>
+            <div class="talking">
+                <figure class="talking-right_icon">
+                    <img src="img/icon_meowth.jpg" alt="" class="image_m">
+                    <figcaption>aSONY</figcaption>
+                </figure>
+                <div class="talking-right">
+                    <p>○○○○○○○○○○○○、<br>
+                        ○○○○○○○○○○○○ですよ。</p>
+                </div>
+            </div>
+        </dd>
+    </dl>
+    <dl class="qa">
+        <dt>○○○○○○○○○○○○について</dt>
+        <dd>
+            <div class="talking">
+                <figure class="talking-left_icon">
+                    <img src="img/icon_psyduck.jpg" alt="" class="image_p">
+                    <figcaption>user</figcaption>
+                </figure>
+                <div class="talking-left">
+                    <p>○○○○○○○○○○○○、<br>
+                        ○○○○○○○○○○○○ですか？</p>
+                </div>
+            </div>
+            <div class="talking">
+                <figure class="talking-right_icon">
+                    <img src="img/icon_meowth.jpg" alt="" class="image_m">
+                    <figcaption>aSONY</figcaption>
+                </figure>
+                <div class="talking-right">
+                    <p>○○○○○○○○○○○○、<br>
+                        ○○○○○○○○○○○○ですよ。</p>
+                </div>
+            </div>
+        </dd>
+    </dl>
+    <dl class="qa">
+        <dt>○○○○○○○○○○○○について</dt>
+        <dd>
+            <div class="talking">
+                <figure class="talking-left_icon">
+                    <img src="img/icon_psyduck.jpg" alt="" class="image_p">
+                    <figcaption>user</figcaption>
+                </figure>
+                <div class="talking-left">
+                    <p>○○○○○○○○○○○○、<br>
+                        ○○○○○○○○○○○○ですか？</p>
+                </div>
+            </div>
+            <div class="talking">
+                <figure class="talking-right_icon">
+                    <img src="img/icon_meowth.jpg" alt="" class="image_m">
+                    <figcaption>aSONY</figcaption>
+                </figure>
+                <div class="talking-right">
+                    <p>○○○○○○○○○○○○、<br>
+                        ○○○○○○○○○○○○ですよ。</p>
+                </div>
+            </div>
+        </dd>
+    </dl>
+</div>
+<h2>よくある質問にない場合はこちらへご連絡ください。<br>
+    ○○○○○○@○○○○.○○</h2>
+<br>
+<a href="../top/toppage.php">
+    <button type="button" name="top_button"><h2>トップページ</h2></button>
+</a>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="Q&A.js"></script>
+</div>
+<?php require '../footer.php';?>
