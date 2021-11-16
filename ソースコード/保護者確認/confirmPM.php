e602bb517ae81105add3b90fabbb0576a08d51fa
aso2001036 <83050405+aso2001036@users.noreply.github.com>
2021-11-16 10:20:07 +0900

    Add files via upload

---
+++ ソースコード/保護者確認/confirmPM.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+?>
+<?php require '../header.php'; ?>
+    <div class = "log-form">
+        <h3>ご案内</h3>
+        <img src = "親子.png" name = "1">
+        <h4>ほごしゃの方といっしょに見ようね！</h4>
+        <a href = ""><input type = "button" value = "次へ"></a>
+    </div>
+<?php require '../footer.php';?>
