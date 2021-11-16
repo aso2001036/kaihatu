20541fb696eddafb348b9eea4b36bb9deef544df
aso2001036 <83050405+aso2001036@users.noreply.github.com>
2021-11-16 10:28:28 +0900

    Add files via upload

---
+++ ソースコード/ログイン選択/login-screen.php
@@ -0,0 +1,12 @@
+<?php require '../header.php'; ?>
+<div class="content">
+    <a href="../login/login-in.php">
+        <button type="button">ログイン</button>
+    </a>
+    <br>
+    <br>
+    <a href="../input-mail/MailInput.php">
+        <button type="button" name="login-button">新規登録</button>
+    </a>
+</div>
+<?php require '../footer.php'; ?>
