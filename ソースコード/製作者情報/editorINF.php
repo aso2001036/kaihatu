0e1c7299b3e2945e08bac2feca3ecd1e84de636d
aso2001036 <83050405+aso2001036@users.noreply.github.com>
2021-11-16 10:56:55 +0900

    Add files via upload

---
+++ ソースコード/製作者情報/editorINF.php
@@ -0,0 +1,23 @@
+<?php require '../header.php';?>
+    <div class = "log-form">
+        <h1>制作者情報</h1>
+
+        <h3>開発チーム</h3>
+        <h5>SD2A.2班</h5>
+
+        <h3>チームリーダー</h3>
+        <h5>小野弘之</h5>
+
+        <h3>副チームリーダー</h3>
+        <h5>松石泰紀</h5>
+
+        <h3>チームメンバー</h3>
+        <h5>上山　大輝</h5>
+        <h5>冨田　悠人</h5>
+        <h5>橋田　武蔵</h5>
+        <h5>山下　秀斗</h5>
+        <form action="../top/toppage.php" method = "post">
+            <button type = "submit" name="button"><h2>トップページ</h2></button>
+        </form>
+    </div>
+<?php require '../footer.php';?>
