9bc6579f725ab7556ffe898ba4d02553178768fc
aso2001036 <83050405+aso2001036@users.noreply.github.com>
2021-11-16 10:53:05 +0900

    Add files via upload

---
+++ ソースコード/購入確認/confirmPAS.php
@@ -0,0 +1,11 @@
+<?php require '../header.php';?>
+    <div class = "log-form">
+        <h2>保護者確認ページ</h2>
+        <img src = "main.jpg" width = "250">
+        <h6>現在のパスワードを入力してください</h6>
+        <form action = "" method = "post">
+            <p>PASS : <input type = "password" name = "pas" placeholder = "パスワード"></p>
+            <button type = "submit" name="k_button">確定</button>
+        </form>
+    </div>
+<?php require '../footer.php';?>
