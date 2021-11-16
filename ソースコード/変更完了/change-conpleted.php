cc216034be02562e2cdf0f50bb6683b06c08396c
aso2001036 <83050405+aso2001036@users.noreply.github.com>
2021-11-16 10:58:03 +0900

    Add files via upload

---
+++ ソースコード/変更完了/change-conpleted.php
@@ -0,0 +1,8 @@
+<?php require '../header.php'; ?>
+<div class = "log-form">
+    <h6>登録情報の変更完了しました。</h6>
+    <form action = "" method = "post">
+        <button type = "submit">トップページ</button>
+    </form>
+</div>
+<?php require '../footer.php'; ?>
