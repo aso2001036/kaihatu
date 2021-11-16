7317bbdcab75cdec162a3f73fd45dd5cbdbdde7f
aso2001036 <83050405+aso2001036@users.noreply.github.com>
2021-11-16 10:54:04 +0900

    Add files via upload

---
+++ ソースコード/購入完了/confirmCONP.php
@@ -0,0 +1,9 @@
+<?php require '../header.php';?>
+    <div class = "log-form">
+        <img src = "main.jpg" width = "250">
+        <h6>ご購入ありがとうございました。</h6>
+        <form action = "" method = "post">
+            <button type = "submit" name="k_button">トップページ</button>
+        </form>
+    </div>
+<?php require '../footer.php';?>
